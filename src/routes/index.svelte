7afd31eabdc7886e4856f49d49a6af4201a90af6
Chris Ha <chunghha@users.noreply.github.com>
2022-07-22 15:33:43 -0500

    chore: import the fonts' css for vite 3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,9 +5,9 @@
 <script lang="ts">
 	import CountryCard from '$lib/components/countrycard.svelte';
 	import type { Country } from '$lib/models/country';
-	import '@fontsource/inter';
-	import '@fontsource/poppins';
-	import '@fontsource/space-mono';
+	import '@fontsource/inter/index.css';
+	import '@fontsource/poppins/index.css';
+	import '@fontsource/space-mono/index.css';
 	import { fetchCountries } from '../utils';
 
 	let countries: Country[] = [];
