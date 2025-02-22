d805e6bdf6ce7b716bacc2e65ae6fda8005d54f1
Chris Ha <chunghha@users.noreply.github.com>
2025-02-21 23:33:34 -0600

    build(lib): tailwindCSS v4 / daisyUI v5

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,7 @@
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
-<h1 class="text-poppins my-8 text-center text-4xl uppercase">About</h1>
-<p class="text-inter text-center">This is a SvelteKit demo that uses the Country API.</p>
+<h1 class="text-tienne my-8 text-center text-4xl uppercase">About</h1>
+<p class="text-mulish text-center">
+  This is a SvelteKit demo that uses the Country API.
+</p>
